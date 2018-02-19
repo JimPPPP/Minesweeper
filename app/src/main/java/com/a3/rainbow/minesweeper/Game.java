cffe922f24cf518381e8f6b2fc75ffec30e85878
Rainbow <git@csil-git1.cs.surrey.sfu.ca:rll7/cmpt276-a2.git>
2018-02-18 18:21:49 -0800

    game layout done, functionality needed

--- app/src/main/java/com/a3/rainbow/minesweeper/Game.java
+++ app/src/main/java/com/a3/rainbow/minesweeper/Game.java
@@ -1,18 +1,25 @@
 package com.a3.rainbow.minesweeper;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.a3.rainbow.minesweeper.logic.Star;
 
 import java.util.Locale;
 
 public class Game extends AppCompatActivity {
-
-    private static final int num_rows = 0;
-
     private Star starfield;
+    private int num_rows = 0;
+    private int num_cols = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +28,29 @@ public class Game extends AppCompatActivity {
 
         starfield = Star.getInstance();
 
+        int sizeints = Integer.parseInt(getBoardSize(this).replaceAll("[\\D]", ""));
+        String intStr = Integer.toString(sizeints);
+        String num_rows_str = Character.toString(intStr.charAt(0));
+        num_rows = Integer.parseInt(num_rows_str);
 
+        String num_cols_str = Character.toString(intStr.charAt(1));
+        if (intStr.length() != 2) {
+            num_cols_str += Character.toString(intStr.charAt(2));
+        }
+        num_cols = Integer.parseInt(num_cols_str);
 
-       // populateButtons();
+        Button buttons [][] = newButtons();
+
+        populateButtons(buttons);
       //  updateUI();
+    }
+
+    public Button [][] newButtons() {
+        return new Button[num_cols][num_rows];
+    }
+
+    public void setButton(Button[][] buttons, Button button, int cols, int rows){
+        buttons[cols][rows] = button;
     }
 
 //    private int starNum = starfield.getNumStars();
@@ -39,11 +65,61 @@ public class Game extends AppCompatActivity {
         String scanMsg = String.format(Locale.getDefault(), "%d scans used.", scanNum);
         scans.setText(scanMsg);
     }
+    */
 
-    private void populateButtons() {
+    private void populateButtons(final Button[][] buttons) {
+        TableLayout table  = findViewById(R.id.starfield);
+
         for (int row = 0; row < num_rows; row++) {
+            TableRow trow = new TableRow(this);
+            trow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f
+            ));
+            table.addView(trow);
 
+            for (int col = 0; col < num_cols; col++) {
+                final int row_final = row;
+                final int col_final = col;
+
+                final Button button = new Button(this);
+                button.setBackgroundResource(R.drawable.btnbg);
+                button.setTextColor(R.color.white);
+                button.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1.0f
+                ));
+
+                setButton(buttons, button, col_final, row_final);
+
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        gridButtonClicked(col_final, row_final, buttons);
+                    }
+                });
+
+                trow.addView(button);
+            }
         }
     }
-    */
+
+    private void gridButtonClicked(int row, int col, Button [][] buttons) {
+        Button button = buttons[row][col];
+        button.setText("X");
+        button.setBackgroundResource(R.drawable.btnbgclicked);
+    }
+
+
+    private String getBoardSize(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        return prefs.getString("Board size", "");
+    }
+
+    static public int getStarNum(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        return prefs.getInt("Number of stars", 0);
+    }
 }
--- app/src/main/java/com/a3/rainbow/minesweeper/Options.java
+++ app/src/main/java/com/a3/rainbow/minesweeper/Options.java
@@ -1,13 +1,20 @@
 package com.a3.rainbow.minesweeper;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.support.v4.widget.CompoundButtonCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatRadioButton;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class Options extends AppCompatActivity {
 
@@ -18,7 +25,11 @@ public class Options extends AppCompatActivity {
 
         createBoardBtns();
         createSizeBtns();
+
+        int savedStarNum = getStarNum(this);
+        String savedBoardSize = getBoardSize(this);
     }
+
 
     //TODO: add erase button with functionality
 
@@ -36,14 +47,25 @@ public class Options extends AppCompatActivity {
         String[] numSize = getResources().getStringArray(R.array.board_size);
 
             for (int i = 0; i < numSize.length; i++) {
-                String sizeNum = numSize[i];
+                final String sizeNum = numSize[i];
 
                 RadioButton button = new RadioButton(this);
                 button.setText(sizeNum + "");
                 button.setTextColor(Color.WHITE);
                 CompoundButtonCompat.setButtonTintList(button, colorsl);
 
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        saveSize(sizeNum);
+                    }
+                });
+
                 sizeGroup.addView(button);
+
+                if (Objects.equals(sizeNum, getBoardSize(this))) {
+                    button.setChecked(true);
+                }
             }
     }
 
@@ -51,17 +73,50 @@ public class Options extends AppCompatActivity {
         RadioGroup starGroup = findViewById(R.id.star_group);
         int[] numStars = getResources().getIntArray(R.array.num_stars);
 
-
-
         for (int i = 0; i < numStars.length; i++) {
-            int starNum = numStars[i];
+            final int starNum = numStars[i];
 
             AppCompatRadioButton button = new AppCompatRadioButton(this);
             button.setText(starNum + "");
             button.setTextColor(Color.WHITE);
             CompoundButtonCompat.setButtonTintList(button, colorsl);
 
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    saveStars(starNum);
+                }
+            });
+
             starGroup.addView(button);
+
+            if (starNum == getStarNum(this)) {
+                button.setChecked(true);
+            }
         }
+    }
+
+    private void saveSize(String sizeNum) {
+        SharedPreferences prefs = this.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("Board size", sizeNum);
+        editor.apply();
+    }
+
+    private void saveStars(int starNum) {
+        SharedPreferences prefs = this.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("Number of stars", starNum);
+        editor.apply();
+    }
+
+    private String getBoardSize(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        return prefs.getString("Board size", "");
+    }
+
+    static public int getStarNum(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        return prefs.getInt("Number of stars", 0);
     }
 }
