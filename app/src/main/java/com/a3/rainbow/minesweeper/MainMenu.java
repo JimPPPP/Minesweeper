5ebfb0e21ab4f75c53f18cc8bb775a5ed2e9af8d
Rainbow <git@csil-git1.cs.surrey.sfu.ca:rll7/cmpt276-a2.git>
2018-02-18 20:03:04 -0800

    base starfield done, need to add proper scanning

--- app/src/main/java/com/a3/rainbow/minesweeper/MainMenu.java
+++ app/src/main/java/com/a3/rainbow/minesweeper/MainMenu.java
@@ -1,6 +1,8 @@
 package com.a3.rainbow.minesweeper;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,16 +12,29 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.a3.rainbow.minesweeper.logic.Star;
+
 public class MainMenu extends AppCompatActivity {
+    private Star starfield;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        starfield = Star.getInstance();
+
+        int savedStarNum = getStarNum(this);
+        starfield.setNumStars(savedStarNum);
+
         setupGameBtn();
         setupOptionsBtn();
         setupHelpBtn();
+    }
+
+    static public int getStarNum(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("AppPrefs", MODE_PRIVATE);
+        return prefs.getInt("Number of stars", 0);
     }
 
     private void setupGameBtn() {
@@ -29,6 +44,8 @@ public class MainMenu extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(MainMenu.this, Game.class);
+                int savedStarNum = getStarNum(getBaseContext());
+                starfield.setNumStars(savedStarNum);
                 startActivity(intent);
             }
         });
